828baeb2e4fba394c83b95cec5ec183c3140ecab
Leandro Dias <leandrodias.me@gmail.com>
2021-03-03 19:44:15 -0400

    Third Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.send("Hello world");
+  return res.send("Hello world!");
 });
 
 app.listen(3000, () => {
