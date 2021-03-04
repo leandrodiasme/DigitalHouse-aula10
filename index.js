5dc53747c3f5bc7b7abd44888eed046aedf9008c
Leandro Dias <leandrodias.me@gmail.com>
2021-03-03 20:46:21 -0400

    Last version

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Hello world!");
+const usuarios = ["Leandro", "José", "João"];
+
+// Root Params
+app.get("/usuarios", (req, res) => {
+  return res.json(usuarios);
+});
+
+// Query Params
+app.get("/users", (req, res) => {
+  const { nome } = req.query;
+  let listaRetorno = usuarios.filter((i) =>
+    i.includes(nome || "Não encontrado")
+  );
+  return res.json(listaRetorno);
+});
+
+app.get("/:usuario", (req, res) => {
+  const { usuario } = req.params;
+  return res.send("Hello world!" + usuario);
 });
 
 app.listen(3000, () => {
